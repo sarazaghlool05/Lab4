40ae86e28916ce0516da26d1f38b02e2342884cb
sarazaghlool <68552608+sarazaghlool@users.noreply.github.com>
2025-10-15 20:56:00 +0300

    Update EmployeeRole.java

--- employee/src/EmployeeRole.java
+++ employee/src/EmployeeRole.java
@@ -2,9 +2,13 @@ public class EmployeeRole {
     private ProductDatabase productsDatabase;
     private CustomerProductDatabase customerProductDatabase;
 
+    //do we need getters and setters?
+
     public EmployeeRole() {
-        productsDatabase = new ProductDatabase();
-        customerProductDatabase = new CustomerProductDatabase();
+        productsDatabase = new ProductDatabase("Products.txt");
+        customerProductDatabase = new CustomerProductDatabase("customerProducts.txt");
+        productsDatabase.readFromFile();
+        customerProductDatabase.readFromFile();
     }
 
 }
