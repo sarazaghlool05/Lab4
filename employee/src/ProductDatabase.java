9ebe0c95b1ad70c406e96904e97509412bb8b1e1
sarazaghlool <68552608+sarazaghlool@users.noreply.github.com>
2025-10-15 16:21:00 +0300

    Update ProductDatabase.java

--- employee/src/ProductDatabase.java
+++ employee/src/ProductDatabase.java
@@ -5,4 +5,27 @@ public class ProductDatabase {
     public void readFromFile() {
         //code
     }
+    public ArrayList<Product> returnAllProducts() {
+        //code
+        return null;
+    }
+    public Product createRecordFrom(String line){
+        //code
+        return null;
+    }
+    public boolean contains(String key ){
+        return false;
+    }
+    public Product getRecord(String key){
+        return null;
+    }
+    public void insertRecord(Product record){
+        //code
+    }
+    public void deleteRecord(String key){
+        //code
+    }
+    public void saveToFile(){
+        //code
+    }
 }
