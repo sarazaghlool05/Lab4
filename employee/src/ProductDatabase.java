61ac10eec3992d5fc892f4f37e7d0a7d13ef3238
Bassantzidan <es.bassant.salah2023@alexu.edu.eg>
2025-10-16 00:06:06 +0300

    created (ProductDatabase) class

---
+++ employee/src/ProductDatabase.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+//written by Bassant on wednesday 15/10 @23:50
+public class ProductDatabase {
+    private ArrayList<Product> records;
+    private String filename;
+    public ProductDatabase (String filename){
+        this.filename=filename;
+    }
+    public void readFromFile(){
+        
+    }
+}
