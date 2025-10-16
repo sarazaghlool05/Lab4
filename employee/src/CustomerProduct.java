a83e6520fd81bd4fce11eba7d2432e79b87dc28d
zeinaelbanna <zeinaelbanna5@gmail.com>
2025-10-16 12:39:54 +0300

    finish Customer Product class

--- employee/src/CustomerProduct.java
+++ employee/src/CustomerProduct.java
@@ -2,17 +2,17 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class CustomerProduct {
-     private String customerSSN;
-     private String productID;
-     private LocalDate purchaseDate;
-     private boolean paid;
-     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private String customerSSN;
+    private String productID;
+    private LocalDate purchaseDate;
+    private boolean paid;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-     public CustomerProduct(String customerSSN, String productID, LocalDate purchaseDate){
+    public CustomerProduct(String customerSSN, String productID, LocalDate purchaseDate) {
         this.customerSSN = customerSSN;
         this.productID = productID;
         this.purchaseDate = purchaseDate;
-         this.paid = false;
+        this.paid = false;
     }
 
     public String getCustomerSSN() {
@@ -20,26 +20,26 @@ public class CustomerProduct {
     }
 
     public String getProductID() {
-         return productID;
+        return productID;
     }
 
     public LocalDate getPurchaseDate() {
-         return purchaseDate;
+        return purchaseDate;
     }
 
     public boolean isPaid() {
-         return paid;
+        return paid;
     }
 
     public void setPaid(boolean paid) {
-         this.paid = paid;
+        this.paid = paid;
     }
 
     public String lineRepresentation() {
-         return customerSSN + "," + productID + "," + purchaseDate.format(formatter) + "," + paid;
-     }
+        return customerSSN + "," + productID + "," + purchaseDate.format(formatter) + "," + paid;
+    }
 
-    public String getSearchKey(){
+    public String getSearchKey() {
         return customerSSN + "," + productID + "," + purchaseDate.format(formatter);
     }
 }
