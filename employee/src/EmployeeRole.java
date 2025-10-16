faaf68ea8fb49807a080b2571c0370c33437ac63
sarazaghlool <68552608+sarazaghlool@users.noreply.github.com>
2025-10-16 16:19:01 +0300

    Update EmployeeRole.java

--- employee/src/EmployeeRole.java
+++ employee/src/EmployeeRole.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +19,7 @@ public class EmployeeRole {
         customerProductDatabase.readFromFile();
     }
 
+    //written by sara zaghlool on wednesday 15/10
     //adding a new product to the file
     public void addProduct(String productID, String productName, String manufacturerName, String supplierName, int quantity, float price) {
         //ask why the price is not included in the function on the lab4 pdf
@@ -31,6 +33,7 @@ public class EmployeeRole {
         }
     }
 
+    //written by sara zaghlool on wednesday 15/10
     //making a new array filled with all products in the files
     public Product[] getListOfProducts(){
         ArrayList<Product> products = new ArrayList<Product>();
@@ -52,6 +55,7 @@ public class EmployeeRole {
         return productArray;
     }
 
+    //written by sara zaghlool on wednesday 15/10
     public CustomerProduct[] getListOfPurchasingOperations(){
         ArrayList<CustomerProduct> purchasedProducts = new ArrayList<CustomerProduct>();
         try(BufferedReader br = new BufferedReader(new FileReader("CustomersProducts.txt"))){
@@ -75,6 +79,7 @@ public class EmployeeRole {
         return new CustomerProduct[0];
     }
 
+    //written by sara zaghlool on friday 16/10
     public boolean purchaseProduct(String customerSSN, String productID, LocalDate purchaseDate){
         List<String> lines = new ArrayList<String>();
         int flag = 0;
@@ -122,5 +127,82 @@ public class EmployeeRole {
         }
         System.out.println("item not found");
         return false;
+    }
+
+
+    //written by sara zaghlool on friday 16/10
+    public double returnProduct(String customerSSN, String productID, LocalDate purchaseDate , LocalDate returnDate){
+        if(returnDate.isBefore(purchaseDate)){
+            return -1;
+        }
+        else if(ChronoUnit.DAYS.between(purchaseDate, returnDate) > 14){
+            return -1;
+        }
+
+        int flag = 0;
+        List<String> linesC = new ArrayList<String>();
+        String lineC;
+        try(BufferedReader br = new BufferedReader(new FileReader("CustomersProducts.txt"))){
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+            String target = customerSSN + "," + productID + "," + purchaseDate.format(formatter);
+            while ((lineC = br.readLine()) != null) {
+                if (!lineC.startsWith(target)) {
+                    linesC.add(lineC);
+                } else {
+                    flag = 1;
+                }
+            }
+        }catch (IOException e) {
+            System.out.println("error in reading from file(from employee role 5th method)");
+        }
+
+        double price = -1;
+        boolean foundProduct = false;
+        if(flag == 1){
+            List<String> lines = new ArrayList<String>();
+            try(BufferedReader br = new BufferedReader(new FileReader("Products.txt"))){
+                String line;
+                while((line = br.readLine()) != null){
+                    String[] words = line.trim().split("\\s*,\\s*");
+                    if(words[0].equals(productID)){
+                        foundProduct = true;
+                        int quantity = Integer.parseInt(words[4]);
+                        ++quantity;
+                        price = Double.parseDouble(words[5]);
+                        words[4] = Integer.toString(quantity);
+                        line = String.join(",",words);
+                    }
+                    lines.add(line);
+                }
+            } catch (IOException e) {
+                System.out.println("error in reading from file(from employee role 5th method)");
+            }
+            if(!foundProduct){
+                return -1;
+            }
+            if(foundProduct){
+                try(BufferedWriter bw = new BufferedWriter(new FileWriter("CustomersProducts.txt"))){
+                    for (int i = 0; i < linesC.size(); i++) {
+                        bw.write(linesC.get(i));
+                        bw.newLine();
+                    }
+                }catch (IOException e) {
+                    System.out.println("error in writing to file(from employee role 5th method)");
+                }
+            }
+
+            try(BufferedWriter bw = new BufferedWriter(new FileWriter("Products.txt"))){
+                for (int i = 0; i < lines.size(); i++) {
+                    bw.write(lines.get(i));
+                    bw.newLine();
+                }
+            }catch (IOException e) {
+                System.out.println("error in writing to file(from employee role 5th method)");
+            }
+        }
+        else{
+            return -1;
+        }
+        return price;
     }
 }
