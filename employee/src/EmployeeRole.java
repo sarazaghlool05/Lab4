84e8c649020f43e5bc1b60403cf8fdc2113c6809
sarazaghlool <68552608+sarazaghlool@users.noreply.github.com>
2025-10-16 16:20:43 +0300

    Update EmployeeRole.java

--- employee/src/EmployeeRole.java
+++ employee/src/EmployeeRole.java
@@ -205,4 +205,49 @@ public class EmployeeRole {
         }
         return price;
     }
+
+    //written by sara zaghlool on friday 16/10
+    public boolean applyPayment(String customerSSN, LocalDate purchaseDate){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String date = purchaseDate.format(formatter);
+
+        boolean flag = false;
+        List<String> lines = new ArrayList<String>();
+        try(BufferedReader br = new BufferedReader(new FileReader("CustomersProducts.txt"))){
+            String line;
+            while((line = br.readLine()) != null){
+                String[] words = line.trim().split("\\s*,\\s*");
+                if(words[0].equals(customerSSN) && words[2].equals(date)){
+                    if(words[3].equals("false")){
+                        words[3] = "true";
+                        flag = true;
+                        line = String.join(",",words);
+                    }
+                }
+                lines.add(line);
+            }
+        }catch(IOException e){
+            System.out.println("error in opening file (from employee role method 6)");
+        }
+        if(!flag){
+            System.out.println("No matching unpaid record found or payment already done.");
+            return flag;
+        }
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter("CustomersProducts.txt"))){
+            for(int i = 0; i < lines.size(); i++){
+                bw.write(lines.get(i));
+                bw.newLine();
+            }
+        }catch(IOException e){
+            System.out.println("error in opening file (from employee role method 6)");
+        }
+        return flag;
+    }
+
+    //written by sara zaghlool on friday 16/10
+    public void logout() {
+        //we will need it in part 2
+        System.out.println("All data already saved. Logging out.");
+    }
+
 }
